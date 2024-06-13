a97285e9e3996a4687f0d53ae5f132e723b815a5
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2024-06-13 14:26:24 +0100

    should auto deploy

--- Data/dbo.Region_Data.sql
+++ Data/dbo.Region_Data.sql
@@ -2,3 +2,4 @@ INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription], [RegionName]) VALUE
 INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription], [RegionName]) VALUES (2, N'Western                                           ', NULL)
 INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription], [RegionName]) VALUES (3, N'Northern                                          ', NULL)
 INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription], [RegionName]) VALUES (4, N'Southern                                          ', NULL)
+INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription], [RegionName]) VALUES (1000, N'blah                                              ', N'poland')
